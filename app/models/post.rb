d3eddd19e7ebb01dc5121a0962aca43ff61d5018
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-03-25 08:47:30 -0300

    ok

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  validates :title, :description, presence: true
+  validates :title, length: { minimum: 5 }
+  validates :description, length: { maximum: 55 }
+
+  before_save do
+    self.title = self.title + " Thiago"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_24_204629) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_25_110950) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
